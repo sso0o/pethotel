b590ef97939edbc65a73affa62e7b2f88ce993a6
choesoyeon <ssssso.yyyy@gmail.com>
2024-11-13 21:06:42 +0900

    로그인 페이지

---
+++ src/main/java/com/example/pethotel/controller/main/MainApiController.java
@@ -0,0 +1,13 @@
+package com.example.pethotel.controller.main;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MainApiController {
+
+
+
+}
--- src/main/java/com/example/pethotel/controller/user/UserViewController.java
+++ src/main/java/com/example/pethotel/controller/user/UserViewController.java
@@ -6,13 +6,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserViewController {
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
 
-    @GetMapping("/signup")
-    public String signup() {
-        return "signup";
-    }
 }
