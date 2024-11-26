adfab1e9cd26e342bf251e4f2bf98a1e100f137a
choesoyeon <ssssso.yyyy@gmail.com>
2024-11-26 18:30:09 +0900

    무한스크롤로 구현 예정

--- src/main/resources/static/manager/myHotel.js
+++ src/main/resources/static/manager/myHotel.js
@@ -1,24 +1,23 @@
-
 // 호텔 등록 및 수정 모달 오픈
-function addHotelModal(hotelId){
+function addHotelModal(hotelId) {
 
-    if(hotelId === "add") {
+    if (hotelId === "add") {
         // 처음 초기화
         document.getElementById('hotelForm').reset();
         // 추가모드
         $("#saveBtn").show();
         $("#modifyBtn").hide();
         $("#deleteBtn").hide();
-    }else{
+    } else {
         // 수정 or 삭제모드
         $("#saveBtn").hide();
         $("#modifyBtn").show();
         $("#deleteBtn").show();
 
         $.ajax({
-            url:'/manager/myhotel/'+BigInt(hotelId),
-            type:'get',
-            success: function (data){
+            url: '/manager/myhotel/' + BigInt(hotelId),
+            type: 'get',
+            success: function (data) {
 
                 $('#hotelId').val(data.hotel.hotelId);
                 $('#hotelName').val(data.hotel.hotelName);
@@ -36,7 +35,7 @@ function addHotelModal(hotelId){
     myModal.show();
 }
 
-function saveHotel(type){
+function saveHotel(type) {
 
     let url;
     let data;
@@ -52,24 +51,24 @@ function saveHotel(type){
     let hotelInfo = $('#hotelInfo').val();
 
 
-    if (type === 'post'){
+    if (type === 'post') {
         url = "/manager/myhotel"
-    } else if (type === "put"){
-        url = "/manager/myhotel/"+BigInt(hotelId);
-    } else if (type === "del"){
+    } else if (type === "put") {
+        url = "/manager/myhotel/" + BigInt(hotelId);
+    } else if (type === "del") {
 
     }
 
     data = JSON.stringify({
-        userId : userId,
+        userId: userId,
         hotelName: hotelName,
-        hotelType : hotelType,
-        postcode : postcode,
-        address : address,
-        detailAddress : detailAddress,
-        extraAddress : extraAddress,
-        hotelPhone : hotelPhone,
-        hotelInfo : hotelInfo
+        hotelType: hotelType,
+        postcode: postcode,
+        address: address,
+        detailAddress: detailAddress,
+        extraAddress: extraAddress,
+        hotelPhone: hotelPhone,
+        hotelInfo: hotelInfo
     })
 
     $.ajax({
@@ -78,13 +77,13 @@ function saveHotel(type){
         async: true,
         contentType: 'application/json',
         data: data,
-        success: function (result){
+        success: function (result) {
             $('#errorDiv').hide();
             $('#errorMsg').text('');
             alert(result.msg)
             location.reload();
         },
-        error: function (request, status, error){
+        error: function (request, status, error) {
             let result = jQuery.parseJSON(request.responseText)
             $('#errorDiv').show();  // 오류 메시지를 표시하는 div를 보이게
             $('#errorMsg').text('');
@@ -95,7 +94,7 @@ function saveHotel(type){
 
 
 // 이미지 테이블에 이미지 렌더링
-function imgRender(data){
+function imgRender(data) {
     let imgData = `
         <div style="display: flex; align-items: end">
             <img src="${data.himgUrl}" alt="Hotel Image" style="max-width: 100px; max-height: 100px;">
@@ -109,20 +108,20 @@ function imgRender(data){
 }
 
 // 호텔 이미지 삭제 요청
-function delHotelImg(himgId){
+function delHotelImg(himgId) {
     // 서버에서 파일 삭제 요청 (예: 서버 API로 삭제)
-    if (confirm("정말 이 사진을 삭제?")){
+    if (confirm("정말 이 사진을 삭제?")) {
         $.ajax({
             url: '/manager/myhotelImg',  // 파일을 삭제할 서버 API 엔드포인트
             type: 'DELETE',  // DELETE 요청
-            data: JSON.stringify({ himgId: String(himgId) }), // 본문에 이미지 URL 포함
+            data: JSON.stringify({himgId: String(himgId)}), // 본문에 이미지 URL 포함
             contentType: 'application/json', // JSON 형식으로 보내기
-            success: function(result) {
+            success: function (result) {
                 alert(result.msg);
                 imgReLoad();
 
             },
-            error: function (request, status, error){
+            error: function (request, status, error) {
                 let result = jQuery.parseJSON(request.responseText)
                 alert(result.msg);
             }
@@ -131,13 +130,13 @@ function delHotelImg(himgId){
 
 }
 
-function imgReLoad(){
+function imgReLoad() {
     let hotelId = $('#hotelId').val();
     // 이미지 로드
     $.ajax({
-        url:'/manager/myhotel/'+hotelId,
+        url: '/manager/myhotel/' + hotelId,
         type: 'get',
-        success:function (data){
+        success: function (data) {
             $('#tabHotelImg').DataTable().clear().rows.add(data.hotel.hotelPhotos).draw();
         }
     })
