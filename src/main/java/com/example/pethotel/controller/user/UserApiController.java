a179457085ccfe10f87dc6b25a04918bdbeab392
choesoyeon <ssssso.yyyy@gmail.com>
2025-01-07 22:03:19 +0900

    :white_check_mark: 비밀번호 확인

--- src/main/java/com/example/pethotel/controller/user/UserApiController.java
+++ src/main/java/com/example/pethotel/controller/user/UserApiController.java
@@ -1,6 +1,7 @@
 package com.example.pethotel.controller.user;
 
 import com.example.pethotel.dto.AddUserRequest;
+import com.example.pethotel.service.CommonService;
 import com.example.pethotel.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -19,19 +20,29 @@ public class UserApiController {
 
     private final UserService userService;
 
-    @PostMapping("/signup")
-    public void signUp(AddUserRequest req, HttpServletResponse response) throws IOException {
-        try {
-            // 회원가입 처리
-            userService.save(req);
+    private final CommonService commonService;
 
-            // 회원가입 성공 후 리다이렉트
-            response.sendRedirect("/login"); // 성공 시 홈으로 리다이렉트
-        } catch (Exception e) {
-            // 오류 발생 시 500 상태 코드로 응답
-            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            response.getWriter().write("회원가입 중 오류가 발생했습니다.");
-        }
+    @PostMapping("/signup")
+    public void signUp(AddUserRequest req) throws Exception {
+
+        commonService.checkRequiredField(req.getUsername(), "이름은 필수 입력 항목입니다.");
+        commonService.checkRequiredField(req.getUserid(), "로그인 아이디는 필수 입력 항목입니다.");
+        commonService.checkRequiredField(req.getPassword(), "비밀번호는 필수 입력 항목입니다.");
+        commonService.checkRequiredField(req.getNickname(), "닉네임은 필수 입력 항목입니다.");
+        commonService.checkRequiredField(req.getUserphone(), "전화번호는 필수 입력 항목입니다.");
+//        try {
+//
+//
+//            // 회원가입 처리
+//            userService.save(req);
+//
+//            // 회원가입 성공 후 리다이렉트
+//            response.sendRedirect("/login"); // 성공 시 홈으로 리다이렉트
+//        } catch (Exception e) {
+//            // 오류 발생 시 500 상태 코드로 응답
+//            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+//            response.getWriter().write("회원가입 중 오류가 발생했습니다.");
+//        }
     }
 
 
